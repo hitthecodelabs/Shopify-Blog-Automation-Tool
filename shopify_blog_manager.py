758078fb49cf058c0c207a2828e5a4b60eba48c6
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2023-12-21 16:16:51 -0500

    Update shopify_blog_manager.py

--- shopify_blog_manager.py
+++ shopify_blog_manager.py
@@ -1,30 +1,34 @@
-import requests
 import json
+import requests
+from tenacity import (
+    retry, wait_random_exponential, stop_after_attempt, retry_if_exception_type
+)
 
-store_url = "https://yourstorename.myshopify.com"
-access_token = "youraccesstoken"
-blog_id = "actualblogID"  # Replace with the actual blog ID
+store_url = "https://example.myshopify.com"
+access_token = "your_access_token"
+blog_id = "your_blog_id"  # Replace with the actual blog ID
+
 headers = {
     "X-Shopify-Access-Token": access_token,
     "Content-Type": "application/json"
 }
 
-def get_blogs():
-    # Retrieves all blogs from the Shopify store.
+@retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
+def get_blogs(store_url, access_token):
+    pass
+        
+@retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
+def create_blog_with_metafields(store_url, access_token, title, key, value, type, namespace):
     pass
 
-def create_blog_with_metafields(title, key, value, type, namespace):
-    # Creates a new blog with specified metafields.
+@retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
+def create_article(store_url, access_token, blog_id, title, author, tags, body_html):
     pass
 
-def create_article(title, author, tags, body_html):
-    # Creates a new article in a specified blog.
+@retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
+def get_articles(store_url, access_token, blog_id):
     pass
-
-def get_articles():
-    # Retrieves all articles from a specified blog.
-    pass
-
-def get_article_count():
-    # Retrieves the count of articles in a specified blog.
+    
+@retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
+def get_article_count(store_url, access_token, blog_id):
     pass
