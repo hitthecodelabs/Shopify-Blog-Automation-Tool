c144463d0a159bca44aaf21fa159aa40e3dd2653
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2023-12-20 22:02:57 -0500

    Update shopify_blog_manager.py

--- shopify_blog_manager.py
+++ shopify_blog_manager.py
@@ -8,3 +8,23 @@ headers = {
     "X-Shopify-Access-Token": access_token,
     "Content-Type": "application/json"
 }
+
+def get_blogs():
+    # Retrieves all blogs from the Shopify store.
+    pass
+
+def create_blog_with_metafields(title, key, value, type, namespace):
+    # Creates a new blog with specified metafields.
+    pass
+
+def create_article(title, author, tags, body_html):
+    # Creates a new article in a specified blog.
+    pass
+
+def get_articles():
+    # Retrieves all articles from a specified blog.
+    pass
+
+def get_article_count():
+    # Retrieves the count of articles in a specified blog.
+    pass
