e16563e9f823b042b2e937042ec09025bf0864c5
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2023-12-24 12:07:58 -0500

    Update shopify_blog_manager.py

--- shopify_blog_manager.py
+++ shopify_blog_manager.py
@@ -34,7 +34,12 @@ def get_blogs(store_url, access_token):
     - blogs = get_blogs('https://example.myshopify.com', 'your_access_token')
       print(blogs)  # Prints the list of blogs retrieved from the Shopify store.
     """
-    pass
+    headers = {"X-Shopify-Access-Token": access_token, "Content-Type": "application/json"}
+    response = requests.get(f"{store_url}/admin/api/2023-10/blogs.json", headers=headers)
+    if response.status_code == 200:
+        return response.json()  # Returns the list of blogs
+    else:
+        raise Exception(f"Error: {response.status_code}, {response.text}")  # Raises an exception if something goes wrong
         
 @retry(wait=wait_random_exponential(multiplier=1, min=4, max=10), stop=stop_after_attempt(10), retry=retry_if_exception_type(requests.exceptions.RequestException))
 def create_blog_with_metafields(store_url, access_token, title, key, value, type, namespace):
