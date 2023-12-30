295d7c35142d40da986f0f56517cc13445d21a46
Hit the Code Labs <145279880+hitthecodelabs@users.noreply.github.com>
2023-12-30 07:16:12 -0500

    Update ai_interaction_utils.py

--- ai_interaction_utils.py
+++ ai_interaction_utils.py
@@ -223,3 +223,67 @@ def validate_and_retry_dynamic(client, messages, model, min_features, max_attemp
     # If all attempts fail, raise an error indicating the failure.
     raise ValueError("Maximum attempts reached. Failed to generate valid content.")
 
+def validate_and_retry_guide(client, messages, model, min_steps, max_attempts=3):
+    """
+    Attempts to generate content dynamically using the OpenAI API and validates the resulting JSON
+    structure, focusing on ensuring a minimum number of guide steps. It retries the content
+    generation up to a specified number of attempts if the JSON is not valid or if the guide step
+    count is below the minimum required.
+
+    Parameters:
+    - client: The OpenAI API client instance to use for making requests.
+    - messages: A list of message dicts representing the conversation history for the API request.
+    - model: The identifier of the model to use for the chat completion.
+    - min_steps: The minimum number of guide steps expected in the response.
+    - max_attempts: The maximum number of attempts to make (default is 3).
+
+    Returns:
+    - json_output: The raw JSON output from the API if successful.
+    - info: The parsed JSON data as a Python dictionary if successful.
+
+    The function first attempts to generate content using the provided client, messages, and model.
+    It then checks whether the generated content is valid JSON and contains a minimum number of
+    guide steps, each paired with corresponding content. If the JSON is invalid, or the guide step
+    count is insufficient, it retries the generation and validation process up to the maximum number of
+    attempts specified. If it fails after all attempts, it raises a ValueError.
+
+    Exceptions:
+    - ValueError: Raised when the maximum number of attempts is reached without successful validation.
+    """
+    for attempt in range(max_attempts):
+        try:
+            # Generate content and attempt to parse it as JSON.
+            json_output = generate_content(client, messages, model=model)
+            info = json.loads(json_output)
+            
+            # Validate the presence of required keys in the JSON.
+            required_keys = {"subheader_3", "subheader_3_message", "Konklusjon", "final_sentence"}
+            if not all(key in info for key in required_keys):
+                missing_keys = required_keys - info.keys()
+                print(f"Missing keys: {missing_keys}. Retrying...")
+                continue
+
+            # Identify and count the guide step and content keys.
+            step_keys = [key for key in info if key.startswith("guide_step_") and not key.endswith("_content")]
+            step_content_keys = [key for key in info if key.startswith("guide_step_") and key.endswith("_content")]
+            
+            # Check for a mismatch between guide step titles and their content.
+            if len(step_keys) != len(step_content_keys):
+                print(f"Mismatch in step and content count. Steps: {len(step_keys)}, Contents: {len(step_content_keys)}. Retrying...")
+                continue
+
+            # Validate the number of guide steps against the minimum required.
+            step_count = len(step_keys)
+            if not (min_steps <= step_count):
+                print(f"Incorrect number of guide steps: {step_count}. Retrying...")
+                continue
+            
+            # If validation passes, return the raw JSON and the parsed information.
+            return json_output, info
+            
+        except json.JSONDecodeError:
+            # If JSON decoding fails, print an error message and retry.
+            print(f"Failed to decode JSON. Retrying ({attempt + 1}/{max_attempts})...")
+    
+    # If all attempts fail, raise an error indicating the failure.
+    raise ValueError("Maximum attempts reached. Failed to generate valid content.")
